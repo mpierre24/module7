ada80e2af5ddd65b36f71f610d8b8429f8e370d5
Student <student@Student-iMac-35.local>
2022-09-08 12:31:46 -0400

    Inital py file

---
+++ index.py
@@ -0,0 +1,101 @@
+print( 'Star Wars Quiz!' )
+
+playing = input ('Are you ready to play?')
+
+if playing != 'yes':
+    quit()
+
+print('Feel the Force!')
+
+answer = input("Who is Luke Skywalker's father? ")
+if answer == "Anakin":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What is baby yoda's real name? ")
+if answer == "Grogu":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("Who killed Jabba? ")
+if answer == "Princess Leia":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("Anakin Skywalker grew up to be who? ")
+if answer == "Darth Vader":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("How many children does Darth Vader have ")
+if answer == "Two": 
+    print('Correct')
+elif answer == "2":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("Who is the bounty hunter who catches Han Solo? ")
+if answer == "Boba Fett":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("? ")
+if answer == "Right":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
+answer = input("What species is Chewbacca? ")
+if answer == "Wookie":
+    print('Correct')
+else:
+    print('Incoorect')
+
